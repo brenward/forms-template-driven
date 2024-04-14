13f493c2794c0be0d1cc307e79fcf116c95ffe5d
brenward <bren.ward@gmail.com>
2024-04-14 17:25:28 +0100

    #Using two way binding on a form input

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,6 +9,7 @@ import { NgForm } from '@angular/forms';
 export class AppComponent {
   @ViewChild('f') signupForm: NgForm;
   defaultProperty:string = 'pet';
+  answer:string;
 
   suggestUserName() {
     const suggestedName = 'Superuser';
